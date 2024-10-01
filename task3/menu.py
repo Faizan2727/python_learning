7303357c19f0b8c105b806e27efd8b614ef90163
Faizan2727 <faizanshaikhbmw2727@gmail.com>
2024-10-01 14:53:26 +0530

    add more lines to menu.py

--- task3/menu.py
+++ task3/menu.py
@@ -22,11 +22,11 @@ print("""
 ch = input("Enter your choice: ")
 print(ch)
 
-if (("notepad" in ch) or (editor in ch)) and (not("dont"in ch)):
+if (("notepad" in ch) or ("editor" in ch)) and (not("dont"in ch)):
     os.system("notepad")
-elif int(io) ==2:
+elif (ch) ==2:
     os.system("chrome")
-elif int(io) ==3:
+elif (ch)==3:
     os.system("vlc")
 else:
     print("idk")
@@ -53,10 +53,73 @@ else:
 # IN KEYWORD
 
 # The in keyword is used to check for the existence of a value within a sequence or collection. It is primarily employed in conditional statements, loops, and comprehensions to determine if a specified elements is present in a string, list, tuple, dictionary keys, or other iterable objects.
-
-if linux in s:
+s = "hey"
+if "linux" in s:
     print("ok")
 else:
     print("not")
 
-# As you can see in above image "Linux" in s: demonstartes the use of the in keyword in python to check if the substrings "linux" exits within the string varaible s. If "Linux" is found within s, the condition evaluates to True and the statement print("ok") is excuted, indicated that "linux" is present in the string. 
+# As you can see in above image "Linux" in s: demonstartes the use of the in keyword in python to check if the substrings "linux" exits within the string varaible s. If "Linux" is found within s, the condition evaluates to True and the statement print("ok") is excuted, indicated that "linux" is present in the string.
+
+# Also in above code,  (("notepad" in ch) or ("editor" in ch)) and (not ("dont" in ch)) utilizea the in kwyword in Python for checking substring membership within the string variable ch.
+
+# Its usage are: This expression evaluates to True if either the substrings "notepad" or "editor" is found within the string ch, and simultaneously ensures that the substring "dont" is not present in ch. It combines logical operation (and , or, not) with substrings checks using in.
+
+# LOGICAL OPERATORS
+
+# logical operators (and,or,not) are used to combine and evaluate multiple condition in a single expression
+
+time = 9
+seat = 300
+print(time> 8 and seat >= 200)
+
+# The AND operator in python is used to combine multiple condition, ensuring that all conditions must be true for the entire expression to evalauted to true. If any condition is false the combined expression evaluates to false. This is useful for scenarios where multiple criteria must all be met.
+
+# In above code this expression checks if both condition are true: time is greater than 8 and seat is greater that equal to 200.
+
+# Explanation: The and operator returns True only if both conditions on its sides are True. In this case, time > 8 is true because 9 is greater than 8 and seat >= 200 is True because 300 is greater than or equal to 200. Therefore, the combined expression evalautes to True.
+
+time = 11
+seat = 30
+print(time > 8 and seat >= 200)
+
+# In above code this expression checks if both conditions are true: time is greater than 8 and seat is greater than or equal to 200.
+# Explanation: The and operator returns True only if both conditions on its sides are True. In this case, time > 8 is True because 11 is greater than 8, but seat >= 200 is False because 30 is not greater than or equal to 200. Therefore, the combined expression evaluates to False.
+
+print(not (time > 8))
+
+# The not keyword is used to invert the boolean value of give expression. It turns into False and vice versa.
+
+# Now in above code not (time > 8)
+
+# Usecase: This expression negates the condition time > 8.
+# The not operator inverts the booleans value of the condition. Since time > 8 is true, applying not makes it False. Therefore the expression evalautes to false.
+
+# The OR operator in Python combines multiple codntions, where the entire expression evaluates to true if at least one condition is true. If all condition are false, the combined expression evalautes to false. This is useful for scenerios where meeting any one of multiple criteria is sufficient.
+
+# Now time > 8 or time > 10 or time > 7
+# Usecase: This expression checks if only any conditions are true: Time is greater that 8, 10 or 7.
+# Explaination : The operators returns True if atleast one of the conditions is True. In this case, all conditions (time > 8, time > 10, time >7) are True because 11 is greater than 8,10 and 7. Therefore, the combines expression evaluates to True.
+
+# LAZY OPERATOR
+# The concepts of Lazy evaluation or short-circuiting primarily applies to logical operators like OR. Lazy evaluation allows you to optimize performance by evaluating expressions only as far as necessary to determine the final outcome.
+
+u = 5
+print(u > 4 or print ("yes"))
+
+# The or operator with lazy evaluation is commonly used when you have an expression where the second operand involves a potentially expensive operation(such as a fucntion call or I/O operation)
+
+# Python evalautes the left operand u > 4.
+
+# Since u is 5 and 5 > 4 is True, Python does not need to evalautes the right operand(print("yes")).
+
+# This is becasue in a logical or opration if the left operand is True, the entire expression is alredy determnined to be true, regradless of the right operand.
+
+# Output: In this case because u > 4 is True, The expression u > 4 or print("yes") evalutes to true without executing print ("yes").
+
+#  Truth Table is used to illustrate the output of logical operations for all possible input values. It is particularly useful for understanding how logical operators like and, or and not function.
+
+# The Time complexity refers to the measure of how much run time does it take to run an algorithm, It is used to make informed decision about algorithemic effieceny ensuring that application and systems perform optimally even with larger datasets or more complex computations.
+
+
+
